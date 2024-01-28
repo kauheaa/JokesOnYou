7ace834411d69a7a59d2b8549b6e392f2c9d7d9d
heikkinenlaura <77092247+heikkinenlaura@users.noreply.github.com>
2024-01-28 12:58:10 +0200

    Player movement and animation

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
+    private Animator animator;
+    public float turnSpeed = 1;
+
+    void Start()
+    {
+        // Get the Animator component
+        animator = GetComponent<Animator>();
+    }
 
     void Update()
     {
@@ -18,6 +26,19 @@ public class PlayerMovement : MonoBehaviour
         // Move the player
         transform.Translate(movement * speed * Time.deltaTime);
 
+        // Check if either horizontal or vertical movement is greater than 0
+        bool isRunning = horizontal > 0f || vertical > 0f;
+
+        // Set the "isRunning" parameter in the Animator
+        if (animator != null)
+        {
+            animator.SetBool("isRunning", isRunning);
+        }
+        if (movement != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, turnSpeed * Time.deltaTime);
+        }
+
     }
-  
 }
